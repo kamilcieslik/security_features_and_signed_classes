9457e759c1d59264262ecff513b31ea72a97daf9
Kamil Cieślik <kamil.cieslik@protonmail.com>
2018-05-07 01:37:40 +0200

    Adding cryptography library.

---
+++ src/main/java/com/github/kamilcieslik/academic/app/cryptography_library/KeyGenerator.java
@@ -0,0 +1,40 @@
+package com.github.kamilcieslik.academic.app.cryptography_library;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+
+public class KeyGenerator {
+    private KeyPairGenerator keyGen;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public KeyGenerator(int keyLength) throws NoSuchAlgorithmException {
+        this.keyGen = KeyPairGenerator.getInstance("RSA");
+        this.keyGen.initialize(keyLength);
+    }
+
+    public void createKeys() {
+        KeyPair pair = this.keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return this.privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return this.publicKey;
+    }
+
+    public void writeToFile(String path, byte[] key) throws IOException {
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(key);
+        fos.flush();
+        fos.close();
+    }
+}
