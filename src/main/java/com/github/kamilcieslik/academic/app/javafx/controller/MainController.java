5038cc19d53ff67cc8b102060bac1402c18a1b4e
Kamil Cieślik <kamil.cieslik@protonmail.com>
2018-05-07 02:42:13 +0200

    Adding reading and writing operations.

--- src/main/java/com/github/kamilcieslik/academic/app/javafx/controller/MainController.java
+++ src/main/java/com/github/kamilcieslik/academic/app/javafx/controller/MainController.java
@@ -1,16 +1,32 @@
 package com.github.kamilcieslik.academic.app.javafx.controller;
 
+import com.github.kamilcieslik.academic.app.cryptography_library.EncoderDecoder;
 import com.github.kamilcieslik.academic.app.javafx.CustomMessageBox;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     private CustomMessageBox customMessageBox;
+    private EncoderDecoder encoderDecoder;
 
     @FXML
     private TextField textFieldKeyPath, textFieldFilePath, textFieldEncryptedFilePath;
@@ -21,21 +37,46 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         customMessageBox = new CustomMessageBox("image/app_icon.png");
+        try {
+            encoderDecoder = new EncoderDecoder();
+        } catch (NoSuchProviderException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void buttonChooseEncryptedFilePath_onAction() {
-
+        FileChooser frontCoversFileChooser = new FileChooser();
+        frontCoversFileChooser.setTitle("Wybierz zaszyfrowany plik");
+        frontCoversFileChooser.getExtensionFilters()
+                .add(new FileChooser.ExtensionFilter("pliki tekstowe", "*.txt"));
+        File file = frontCoversFileChooser.showOpenDialog(textAreaEncryptedFileContent.getScene().getWindow());
+        if (file != null) {
+            String filePath = file.toString();
+            readFileContent(textFieldEncryptedFilePath, textAreaEncryptedFileContent, filePath);
+        }
     }
 
     @FXML
     void buttonChooseFilePath_onAction() {
-
+        FileChooser frontCoversFileChooser = new FileChooser();
+        frontCoversFileChooser.setTitle("Wybierz plik");
+        frontCoversFileChooser.getExtensionFilters()
+                .add(new FileChooser.ExtensionFilter("pliki tekstowe", "*.txt"));
+        File file = frontCoversFileChooser.showOpenDialog(textAreaEncryptedFileContent.getScene().getWindow());
+        if (file != null) {
+            String filePath = file.toString();
+            readFileContent(textFieldFilePath, textAreaFileContent, filePath);
+        }
     }
 
     @FXML
     void buttonChooseKeysPath_onAction() {
-
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Wybierz katalog z kluczami");
+        File directory = chooser.showDialog(textAreaEncryptedFileContent.getScene().getWindow());
+        if (directory != null && directory.isDirectory())
+            textFieldKeyPath.setText(directory.toString());
     }
 
     @FXML
@@ -45,7 +86,29 @@ public class MainController implements Initializable {
 
     @FXML
     void buttonEncrypt_onAction() {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Wybierz ścieżkę zapisu zaszyfrowanego pliku");
+        File directory = chooser.showDialog(textAreaEncryptedFileContent.getScene().getWindow());
+        if (directory != null && directory.isDirectory()) {
+            try {
+                encoderDecoder.encryptFile(textFieldFilePath.getText(), directory.toString());
 
+                String fileContent = readTextFromFile(outPutFilePath);
+                if (fileContent != null) {
+                    textAreaAfterChanges.setText(fileContent);
+                }
+            } catch (IOException e) {
+                customMessageBox.showMessageBox(Alert.AlertType.WARNING, "Ostrzeżenie", "Operacja szyfrowania nie powiodła się.", "Powód: " + "błąd odczytu z pliku").showAndWait();
+            } catch (IllegalBlockSizeException e) {
+                customMessageBox.showMessageBox(Alert.AlertType.WARNING, "Ostrzeżenie", "Operacja szyfrowania nie powiodła się.", "Powód: " + "zbyt duży rozmiar pliku.").showAndWait();
+            } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (BadPaddingException e) {
+                customMessageBox.showMessageBox(Alert.AlertType.WARNING, "Ostrzeżenie", "Operacja szyfrowania nie powiodła się.", "Powód: " + "Bład szyfrowania").showAndWait();
+            } catch (InvalidKeyException e) {
+                customMessageBox.showMessageBox(Alert.AlertType.WARNING, "Ostrzeżenie", "Operacja szyfrowania nie powiodła się.", "Powód: " + "Zbyt duży rozmiar pliku").showAndWait();
+            }
+        }
     }
 
     @FXML
@@ -60,6 +123,40 @@ public class MainController implements Initializable {
 
     @FXML
     void buttonGenerateKeys_onAction() {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Wybierz katalog utworzenia kluczy");
+        File directory = chooser.showDialog(textAreaEncryptedFileContent.getScene().getWindow());
+        if (directory != null && directory.isDirectory()) {
+            try {
+                encoderDecoder.createNewKeys(directory.toString());
+            } catch (IOException e) {
+                customMessageBox.showMessageBox(Alert.AlertType.WARNING, "Ostrzeżenie",
+                        "Operacja wygenerowania kluczy nie powiodła się.",
+                        "Powód: " + e.getMessage() + ".").showAndWait();
+            }
+        }
+    }
 
+    private void readFileContent(TextField textFieldFilePath, TextArea textAreaFileContent, String filePath) {
+        BufferedReader bufferedReader;
+
+        try {
+            textFieldFilePath.setText(filePath);
+            bufferedReader = new BufferedReader(new FileReader(filePath));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
+                line = bufferedReader.readLine();
+            }
+
+            textAreaFileContent.setText(stringBuilder.toString());
+        } catch (IOException e) {
+            customMessageBox.showMessageBox(Alert.AlertType.WARNING, "Ostrzeżenie",
+                    "Operacja odczytu z pliku nie powiodła się.", "Powód: " + e.getMessage() + ".")
+                    .showAndWait();
+        }
     }
 }
